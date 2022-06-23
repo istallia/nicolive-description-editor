d2556ee16ea2b06d526557853ccf0b44bfb72b90
Istallia <55042065+istallia@users.noreply.github.com>
2022-06-23 12:40:22 +0900

    Main: 全体的に要素を追加 fontタグ挿入に対応

--- main.js
+++ main.js
@@ -10,21 +10,24 @@ const allowedTags = ['b', 'i', 'u', 's', 'br', 'font'];
 
 
 /* --- 各種タグを挿入する --- */
-const insertTagToSelectedRange = tagName => {
+const insertTagToSelectedRange = (tagName, attributes = {}) => {
 	const selection = window.getSelection();
-	if (!selection.rangeCount || !checkParentId(selection.baseNode.parentNode, 'description-wysiwyg')) return;
+	if (!selection.rangeCount || !checkParentId(selection.focusNode.parentNode, 'description-wysiwyg')) return;
 	const range = selection.getRangeAt(0);
 	if (tagName.length > 0) {
 		/* タグを挿入する場合 */
 		const node     = document.createElement(tagName);
 		node.innerHTML = selection.toString();
+		Object.keys(attributes).forEach(key => {
+			node.setAttribute(key, attributes[key]);
+		});
 		range.deleteContents();
 		range.insertNode(node);
 		range.selectNodeContents(node);
 	} else {
 		/* 装飾を消す場合 */
 		const content  = range.cloneContents().firstChild;
-		let diffNode   = selection.baseNode.nextSibling || selection.baseNode;
+		let diffNode   = selection.focusNode.nextSibling || selection.focusNode;
 		const diffName = (diffNode.tagName || diffNode.parentNode.tagName).toLowerCase();
 		let diffParent = diffNode.parentNode;
 		if (range.toString() === diffNode.textContent || allowedTags.includes(diffName)) {
@@ -53,6 +56,11 @@ document.addEventListener('DOMContentLoaded', () => {
 	document.getElementById('set-underline'    ).addEventListener('click', () => insertTagToSelectedRange('u'));
 	document.getElementById('set-strikethrough').addEventListener('click', () => insertTagToSelectedRange('s'));
 	document.getElementById('reset-style'      ).addEventListener('click', () => insertTagToSelectedRange(''));
+	const colorForm = document.getElementById('font-color');
+	const sizeForm  = document.getElementById('font-size');
+	document.getElementById('set-font-color').addEventListener('click', () => insertTagToSelectedRange('font', {color:colorForm.value}));
+	document.getElementById('set-font-size' ).addEventListener('click', () => insertTagToSelectedRange('font', {size:sizeForm.value}));
+	document.getElementById('set-font-style').addEventListener('click', () => insertTagToSelectedRange('font', {color:colorForm.value, size:sizeForm.value}));
 });
 
 
